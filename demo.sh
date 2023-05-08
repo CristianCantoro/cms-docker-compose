8da240501a9e96491701e6d6c1be98d7f908dc00
Cristian Consonni <cristian@balist.es>
2023-05-08 23:37:38 +0200

    Update demo script

--- demo.sh
+++ demo.sh
@@ -16,26 +16,47 @@ critical() {
 }
 
 die_if_file_exists() {
-	local afile
+	local afile="$1"
+
 	if [ -f "$afile" ]; then
 		critical "File $afile exists, I will not overwrite it. Exiting."
 		exit 1
 	fi
 }
 
-die_if_file_exists '.env'
-cp '.env.sample' '.env'
+function check_dir() {
+  local adir="$1"
 
-die_if_file_exists 'docker/cms-services/configs/cms.conf'
-cp 'docker/cms-services/configs/cms.conf.sample' 'docker/cms-services/configs/cms.conf'
+  if [[ ! -d "$adir" ]]; then
+    (>&2 echo "$adir is not a valid directory.")
+    exit 1
+  fi
+}
 
-die_if_file_exists 'docker/cms-services/configs/cms.ranking.conf'
-cp 'docker/cms-services/configs/cms.ranking.conf.sample' 'docker/cms-services/configs/cms.ranking.conf'
+# die_if_file_exists '.env'
+# cp '.env.sample' '.env'
 
-export CMS_INIT=true
-export CMS_IMPORT_CONTEST=true
+# die_if_file_exists 'docker/cms-services/configs/cms.conf'
+# cp 'docker/cms-services/configs/cms.conf.sample' 'docker/cms-services/configs/cms.conf'
 
-docker compose build
+# die_if_file_exists 'docker/cms-services/configs/cms.ranking.conf'
+# cp 'docker/cms-services/configs/cms.ranking.conf.sample' 'docker/cms-services/configs/cms.ranking.conf'
 
-docker compose up
+# set_default_if_unset
+export CMS_MAKE_TASK=${CMS_MAKE_TASK=true}
+export CMS_INIT=${CMS_INIT=true}
+export CMS_IMPORT_CONTEST=${CMS_IMPORT_CONTEST=contest}
+
+echo "CMS_MAKE_TASK: $CMS_MAKE_TASK"
+echo "CMS_INIT: $CMS_INIT"
+echo "CMS_IMPORT_CONTEST: $CMS_IMPORT_CONTEST"
+
+if [[ -n "${CMS_IMPORT_CONTEST+x}" ]] && check_dir "$CMS_IMPORT_CONTEST"; then
+	docker compose \
+	  run -v "$CMS_IMPORT_CONTEST":/home/cmsuser/contest taskbuilder
+fi
+
+# docker compose build
+
+# docker compose up
 
